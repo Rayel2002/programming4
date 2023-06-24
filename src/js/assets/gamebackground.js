170a36f4eed35d9f22b16328fb5fa0bbf84006f7
Rayel2002 <1002893@hr.nl>
2023-06-24 02:04:58 +0200

    added comments

--- src/js/assets/gamebackground.js
+++ src/js/assets/gamebackground.js
@@ -3,7 +3,11 @@ import { Resources, ResourceLoader } from "../resources.js";
 
 export class GameBackground extends Actor {
         offset
-        
+        constructor() {
+            super({width:Resources.GameBackground.width, height:Resources.GameBackground.height})
+        }
+
+        // voegt de Background toe aan de scene dmv een actor
         onInitialize(engine){
             const spaceImage = Resources.GameBackground.toSprite()
             this.offset = spaceImage.width
@@ -21,12 +25,13 @@ export class GameBackground extends Actor {
                 ]
             })
     
+            // zorgt ervoor dat de achtergrond beweegt
             this.graphics.anchor = new Vector(0,0)
             this.graphics.add(group)       
             this.pos = new Vector(0, 0)
             this.vel = new Vector(-110, 0)
         }
-    
+        // zorgt ervoor dat de achtergrond oneindig doorloopt door hem terug te zetten naar 0 als hij uit het scherm gaat
         onPostUpdate(engine, delta) {
             if (this.pos.x < -this.offset) {
                 this.pos = new Vector(0, 0)
